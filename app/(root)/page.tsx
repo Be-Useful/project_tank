35c2b68a57bb7f246a02330bd38dce2a0a12ff61
Tarun Kumar <85792301+Be-Useful@users.noreply.github.com>
2024-12-26 23:11:52 +0530

    Content Update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -36,7 +36,7 @@ export default async function Home({
 
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for "${query}"` : "All Startups"}
+          {query ? `Search results for "${query}"` : "All Projects"}
         </p>
 
         <ul className="mt-7 card_grid">
