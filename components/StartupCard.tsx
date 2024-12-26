cd689ec128c40ae5349a861fba98d02d81df2208
Tarun Kumar <85792301+Be-Useful@users.noreply.github.com>
2024-12-26 19:38:49 +0530

    revert pagination

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -72,7 +72,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
 export const StartupCardSkeleton = () => (
   <>
-    {[0, 1, 2, 3, 4, 5, 6].map((index: number) => (
+    {[0, 1, 2, 3, 4].map((index: number) => (
       <li key={cn("skeleton", index)}>
         <Skeleton className="startup-card_skeleton" />
       </li>
