368951595418543418786ffdf977d287be251a5e
Tarun Kumar <85792301+Be-Useful@users.noreply.github.com>
2024-12-26 12:27:06 +0530

    fix

--- components/View.tsx
+++ components/View.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Ping from '@/components/Ping';
-import { client } from '@/sanity/lib/client';
-import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
-import { writeClient } from '@/sanity/lib/write-client';
-import {after} from 'next/server';
+import Ping from "@/components/Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
-const view = async ({ id }: { id: string }) => {
-    const { views: totalViews } = await client
-        .withConfig({ useCdn: false })
-        .fetch(STARTUP_VIEWS_QUERY, { id });
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
 
-        after(async () => await writeClient
+  after(
+    async () =>
+      await writeClient
         .patch(id)
-        .set({views: totalViews + 1})
-        .commit());
+        .set({ views: totalViews + 1 })
+        .commit(),
+  );
 
-
-
-    return (
+  return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
         <Ping />
@@ -29,5 +29,4 @@ const view = async ({ id }: { id: string }) => {
     </div>
   );
 };
-
-export default view
+export default View;
